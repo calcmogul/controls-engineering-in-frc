0ff877d5b1538ed408ec9465d8f12ceaae43f1de
Tyler Veness <calcmogul@gmail.com>
2018-07-02 12:51:34 -0700

    Minor cleanups for the probability section

---
+++ code/joint_pdf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from scipy.stats import multivariate_normal
+
+plt.rc("text", usetex=True)
+
+
+def main():
+    x, y = np.mgrid[-1.0:1.0:30j, 0.0:2.0:30j]
+
+    # Need an (N, 2) array of (x, y) pairs.
+    xy = np.column_stack([x.flat, y.flat])
+
+    mu = np.array([0.0, 1.0])
+    sigma = np.array([0.5, 0.5])
+    covariance = np.diag(sigma**2)
+    z = multivariate_normal.pdf(xy, mean=mu, cov=covariance).reshape(x.shape)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot_surface(x, y, z)
+    ax.set_xlabel("$x$")
+    ax.set_ylabel("$y$")
+    ax.set_zlabel("$p(x, y)$")
+
+    plt.savefig("joint_pdf.svg")
+
+
+if __name__ == "__main__":
+    main()
--- code/pdf.py
+++ code/pdf.py
@@ -8,14 +8,12 @@ plt.rc("text", usetex=True)
 
 
 def main():
-    # Plot between -9 and 11 with 0.001 steps
     x_axis = np.arange(-7.5, 9.5, 0.001)
-    # u = 0, omega = 2
-    plt.plot(x_axis, norm.pdf(x_axis, 1, 2))
+    plt.plot(x_axis, norm.pdf(x_axis, loc=1, scale=2))
     plt.axvline(x=2.0, label="$x_1$", color="k", linestyle="--")
     plt.axvline(x=2.5, label="$x_1 + dx_1$", color="k", linestyle="-.")
-    plt.xlabel("x")
-    plt.ylabel("p(x)")
+    plt.xlabel("$x$")
+    plt.ylabel("$p(x)$")
     plt.legend()
     plt.savefig("pdf.svg")
 
