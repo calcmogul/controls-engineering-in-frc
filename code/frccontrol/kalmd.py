958322b07d509846798055ddbd251a3cbfad6c93
Tyler Veness <calcmogul@gmail.com>
2018-07-06 19:53:22 -0700

    Simplified implementation of kalmd.py

--- code/frccontrol/kalmd.py
+++ code/frccontrol/kalmd.py
@@ -4,30 +4,27 @@ import scipy as sp
 
 
 def kalmd(sys, Q, R):
-    """Solves for the steady state kalman gain and covariance matricies.
+    """Solves for the steady state kalman gain and error covariance matrices.
 
     Keyword arguments:
     sys -- discrete state-space model
-    Q -- the model uncertantity
-    R -- the measurement uncertainty
+    Q -- process noise covariance matrix
+    R -- measurement noise covariance matrix
 
     Returns:
-    Kalman gain, covariance.
+    Kalman gain, error covariance matrix.
     """
-    I = np.matrix(np.eye(Q.shape[0]))
-    Z = np.matrix(np.zeros(Q.shape[0]))
-    n = sys.A.shape[0]
-    m = sys.C.shape[0]
+    m = sys.A.shape[0]
 
     observability_rank = np.linalg.matrix_rank(cnt.obsv(sys.A, sys.C))
-    if observability_rank != n:
-        print('warning: Observability of %d != %d, unobservable state',
-              observability_rank, n)
+    if observability_rank != m:
+        print("Warning: Observability of %d != %d, unobservable state",
+              observability_rank, m)
 
-    # Compute the steady state covariance matrix.
+    # Compute the steady state covariance matrix
     P_prior = sp.linalg.solve_discrete_are(a=sys.A.T, b=sys.C.T, q=Q, r=R)
     S = sys.C * P_prior * sys.C.T + R
-    K = np.linalg.lstsq(S.T, (P_prior * sys.C.T).T, rcond=None)[0].T
-    P = (I - K * sys.C) * P_prior
+    K = P_prior * sys.C.T * np.linalg.inv(S)
+    P = (np.eye(m) - K * sys.C) * P_prior
 
     return K, P
