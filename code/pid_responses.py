f80a18feae706e77ceb6789324c7dce55b4541c8
Tyler Veness <calcmogul@gmail.com>
2018-07-08 15:42:20 -0700

    Make book approachable by those who don't know calculus or PID control
    Add calculus crash course to introduction chapter. Also add diagrams to
    the PID controller chapter for teaching PID from scratch.

---
+++ code/pid_responses.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import control as cnt
+from frccontrol import conv
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.rc("text", usetex=True)
+
+
+def make_closed_loop_plant(G, Kp):
+    """Returns a TransferFunction representing a plant in negative feedback with
+    a P controller that uses the given gain.
+
+    Keyword arguments:
+    G -- open-loop plant
+    Kp -- proportional gain
+    """
+    K = cnt.TransferFunction(Kp, 1)
+    return cnt.feedback(G, K)
+
+def sim(tf, T, label):
+    T, yout = cnt.step_response(tf, T=T)
+
+    # 1 / yout[-1] normalizes to DC gain
+    plt.plot(T, yout * 1 / yout[-1], label=label)
+    plt.legend()
+
+
+def main():
+    dt = 0.0001
+    T = np.arange(0, 6, dt)
+
+    plt.xlabel("Time ($s$)")
+    plt.ylabel("Position ($m$)")
+
+    # Make plant
+    G = cnt.TransferFunction(1, conv([1, 5], [1, 0]))
+
+    sim(cnt.TransferFunction(1, 1), T, "Reference")
+    Gcl = make_closed_loop_plant(G, 120)
+    sim(Gcl, T, "Underdamped")
+    Gcl = make_closed_loop_plant(G, 3)
+    sim(Gcl, T, "Overdamped")
+    Gcl = make_closed_loop_plant(G, 6.268)
+    sim(Gcl, T, "Critically damped")
+
+    plt.savefig("pid_responses.svg")
+
+
+if __name__ == "__main__":
+    main()
