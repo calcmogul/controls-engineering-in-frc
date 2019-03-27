3b586429ee9cfcbc9e9f12b927c76db32518fe46
Tyler Veness <calcmogul@gmail.com>
2019-03-26 21:52:39 -0700

    Turn DC brushed motor current limiting remark into a subsection

--- code/snippets/current_limit.py
+++ code/snippets/current_limit.py
@@ -1,6 +1,14 @@
+# Normal control here
+V = ...
+
+# For predictive
+omega = angular velocity measurement
+I = V / R - omega / (Kv * R)
+
+# For reactive
+I = current measurement
+omega = Kv * V - I * R * Kv  # or angular velocity measurement
+
 if I > I_max:
     # Current limiting
     V = I_max * R + omega / Kv
-else:
-    # Normal control here
-    V = ...
