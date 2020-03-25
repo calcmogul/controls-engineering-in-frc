780ede3dc75b647fd1bcf6e06eedff0d12a14f9b
Tyler Veness <calcmogul@gmail.com>
2020-03-24 23:45:14 -0700

    Shade in PDF plot

--- code/pdf.py
+++ code/pdf.py
@@ -17,13 +17,36 @@ plt.rc("text", usetex=True)
 
 
 def main():
-    x = np.arange(-7.5, 9.5, 0.001)
-    plt.plot(x, norm.pdf(x, loc=1, scale=2))
-    plt.axvline(x=2.0, label="$x_1$", color="k", linestyle="--")
-    plt.axvline(x=2.5, label="$x_1 + dx_1$", color="k", linestyle="-.")
+    themecolor = "#386ba6"
+
+    x1 = np.arange(-7.5, 9.5, 0.001)
+    plt.plot(x1, norm.pdf(x1, loc=1, scale=2))
+
+    x2 = np.arange(2.0, 2.5, 0.001)
+    plt.fill_between(
+        x2, [0] * len(x2), norm.pdf(x2, loc=1, scale=2), facecolor=themecolor, alpha=0.5
+    )
+    plt.plot([2.0, 2.0], [0.0, norm.pdf([2.0], loc=1, scale=2)], color=themecolor)
+    plt.plot([2.5, 2.5], [0.0, norm.pdf([2.5], loc=1, scale=2)], color=themecolor)
+
+    # Left arrow
+    plt.annotate(
+        "$x_1$",
+        xy=(2.0, 0.025),  # Start coord of arrow
+        xycoords="data",
+        xytext=(2.0 - 1.0, 0.025),  # End coord of arrow
+        textcoords="data",
+        arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=0"),
+        ha="center",
+        va="center",
+    )
+
+    plt.annotate(
+        "$dx$", xy=(2.25, -0.005), xycoords="data", ha="center", va="center",
+    )
+
     plt.xlabel("$x$")
     plt.ylabel("$p(x)$")
-    plt.legend()
     if "--noninteractive" in sys.argv:
         latex.savefig("pdf")
     else:
