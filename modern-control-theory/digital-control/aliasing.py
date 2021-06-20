f9085f3583d5fcf1e87b4bb81091cbc0be0fec69
Tyler Veness <calcmogul@gmail.com>
2021-06-20 11:32:13 -0700

    Fix aliasing example

--- modern-control-theory/digital-control/aliasing.py
+++ modern-control-theory/digital-control/aliasing.py
@@ -17,26 +17,26 @@ plt.rc("text", usetex=True)
 
 
 def main():
-    xlim = [0, 3]
+    xlim = [0, 4]
     x = np.arange(xlim[0], xlim[1], 0.001)
     plt.xlim(xlim)
 
-    y1 = np.sin(2 * math.pi * x)
-    plt.plot(x, y1, label="Signal at $1$ Hz")
-
-    y2 = np.sin(2 * math.pi * x / 2 + math.pi / 4)
-    plt.plot(x, y2, color="k", linestyle="--", label="Signal at $2$ Hz")
+    y1 = np.sin(2 * math.pi * x * 1.5)
+    y2 = np.sin(-2 * math.pi * x * 0.5)
+    plt.plot(x, y1, label="Original signal at $1.5$ Hz")
 
     # Draw intersection points
     idx = np.argwhere(np.diff(np.sign(y1 - y2)) != 0)
-    plt.plot(x[idx], y1[idx], "ko", label="Samples at $3$ Hz")
+    plt.plot(x[idx], y1[idx], "ko", label="Samples at $2$ Hz")
+
+    plt.plot(x, y2, color="k", linestyle="--", label="Aliased signal at $0.5$ Hz")
 
     plt.xlabel("$t$")
     plt.ylabel("$f(t)$")
     plt.legend()
 
     if "--noninteractive" in sys.argv:
-        latex.savefig("nyquist")
+        latex.savefig("aliasing")
     else:
         plt.show()
 
