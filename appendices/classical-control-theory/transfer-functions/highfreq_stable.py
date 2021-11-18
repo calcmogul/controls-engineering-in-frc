cb6823f42f9604d88a8d7956a2aeca4f6549ae87
Tyler Veness <calcmogul@gmail.com>
2021-11-17 21:51:10 -0800

    Use state-space model instead of transfer function for time domain sim

--- appendices/classical-control-theory/transfer-functions/highfreq_stable.py
+++ appendices/classical-control-theory/transfer-functions/highfreq_stable.py
@@ -16,26 +16,6 @@ import numpy as np
 plt.rc("text", usetex=True)
 
 
-def make_closed_loop_plant(G, Kp):
-    """Returns a TransferFunction representing a plant in negative feedback with
-    a P controller that uses the given gain.
-
-    Keyword arguments:
-    G -- open-loop plant
-    Kp -- proportional gain
-    """
-    K = ct.TransferFunction(Kp, 1)
-    return ct.feedback(G, K)
-
-
-def sim(tf, T, label):
-    T, yout = ct.step_response(tf, T=T)
-
-    # 1 / yout[-1] normalizes to DC gain
-    plt.plot(T, yout * 1 / yout[-1], label=label)
-    plt.legend()
-
-
 def main():
     dt = 0.0001
     T = np.arange(0, 0.25, dt)
@@ -45,28 +25,60 @@ def main():
     b = 3.5077e-6  # N-m-s
     Ke = 0.0181  # V/rad/s
     Kt = 0.0181  # N-m/Amp
-    K = Ke  # Ke = Kt
     R = 0.0902  # Ohms
     L = 230e-6  # H
 
-    # Stable plant (L = 0)
-    # s((Js + b)R + K²)
-    # s(JRs + bR + K²)
-    # JRs² + bRs + K²s
-    # JRs² + (bR + K²)s
-    G = ct.TransferFunction(K, [J * R, b * R + K ** 2, 0])
+    # Transfer function
+    #
+    # s((Js + b)R + Kt Ke)
+    # s(JRs + bR + Kt Ke)
+    # JRs² + bRs + Kt Ke s
+    # JRs² + (bR + Kt Ke)s
+    G = ct.TransferFunction(Ke, [J * R, b * R + Kt * Ke, 0])
+
     ct.root_locus(G, grid=True)
     plt.xlabel("Real Axis (seconds$^{-1}$)")
     plt.ylabel("Imaginary Axis (seconds$^{-1}$)")
     if "--noninteractive" in sys.argv:
         latex.savefig("highfreq_stable_rlocus")
 
+    # State-space
+    #
+    # From sum of forces:
+    #
+    # J ddot{θ} + b dot{θ} = Kt i  (1)
+    #
+    # From Kirchoff's voltage law:
+    #
+    # R i = V - Ke dot{θ}           (2)
+    # i = V/R - Ke/R dot{θ}
+    #
+    # Plug (2) into (1).
+    #
+    # J ddot{θ} + b dot{θ} = Kt (V/R - Kt Ke/R dot{θ})
+    # J ddot{θ} + b dot{θ} = Kt V/R - Kt Ke/R dot{θ}
+    # J ddot{θ} = Kt V/R - Kt Ke/R dot{θ} - b dot{θ}
+    # ddot{θ} = Kt V/(RJ) - Kt Ke/(RJ) dot{θ} - b/J dot{θ}
+    # ddot{θ} = Kt/(RJ) V - (Kt Ke/(RJ) + b/J) dot{θ}
+    # ddot{θ} = -(Kt Ke/(RJ) + b/J) dot{θ} + Kt/(RJ) V
+    A = np.array([[0, 1], [0, -(Kt * Ke / (R * J) + b / J)]])
+    B = np.array([[0], [Kt / (R * J)]])
+    C = np.array([[1, 0]])
+    D = np.array([[0]])
+    K = np.array([[1, 0]])
+    system = ct.StateSpace(A - B @ K, B @ K, C - D @ K, D @ K)
+
     plt.figure(2)
     plt.xlabel("Time ($s$)")
     plt.ylabel("Position ($m$)")
-    sim(ct.TransferFunction(1, 1), T, "Reference")
-    Gcl = make_closed_loop_plant(G, 1)
-    sim(Gcl, T, "Step response")
+
+    plt.plot(T, [1 for t in T], label="Reference")
+    plt.legend()
+
+    _, yout = ct.step_response(system, T=T, input=0, output=0)
+    plt.plot(T, yout, label="Step response")
+    plt.legend()
+
     if "--noninteractive" in sys.argv:
         latex.savefig("highfreq_stable_step")
     else:
--- appendices/classical-control-theory/transfer-functions/highfreq_unstable.py
+++ appendices/classical-control-theory/transfer-functions/highfreq_unstable.py
@@ -16,26 +16,6 @@ import numpy as np
 plt.rc("text", usetex=True)
 
 
-def make_closed_loop_plant(G, Kp):
-    """Returns a TransferFunction representing a plant in negative feedback with
-    a P controller that uses the given gain.
-
-    Keyword arguments:
-    G -- open-loop plant
-    Kp -- proportional gain
-    """
-    K = ct.TransferFunction(Kp, 1)
-    return ct.feedback(G, K)
-
-
-def sim(tf, T, label):
-    T, yout = ct.step_response(tf, T=T)
-
-    # 1 / yout[-1] normalizes to DC gain
-    plt.plot(T, yout * 1 / yout[-1], label=label)
-    plt.legend()
-
-
 def main():
     dt = 0.0001
     T = np.arange(0, 0.25, dt)
@@ -45,28 +25,57 @@ def main():
     b = 3.5077e-6  # N-m-s
     Ke = 0.0181  # V/rad/s
     Kt = 0.0181  # N-m/Amp
-    K = Ke  # Ke = Kt
     R = 0.0902  # Ohms
     L = 230e-6  # H
 
-    # Unstable plant
-    # s((Js + b)(Ls + R) + K²)
-    # s(JLs² + JRs + bLs + bR + K²)
-    # JLs³ + JRs² + bLs² + bRs + K²s
-    # JLs³ + (JR + bL)s² + (bR + K²)s
-    G = ct.TransferFunction(K, [J * L, J * R + b * L, b * R + K ** 2, 0])
+    # Transfer function
+    #
+    # s((Js + b)(Ls + R) + Kt Ke)
+    # s(JLs² + JRs + bLs + bR + Kt Ke)
+    # JLs³ + JRs² + bLs² + bRs + Kt Ke s
+    # JLs³ + (JR + bL)s² + (bR + Kt Ke)s
+    G = ct.TransferFunction(Ke, [J * L, J * R + b * L, b * R + Kt * Ke, 0])
+
     ct.root_locus(G, grid=True)
     plt.xlabel("Real Axis (seconds$^{-1}$)")
     plt.ylabel("Imaginary Axis (seconds$^{-1}$)")
     if "--noninteractive" in sys.argv:
         latex.savefig("highfreq_unstable_rlocus")
 
+    # State-space
+    #
+    # From sum of forces:
+    #
+    # J ddot{θ} + b dot{θ} = Kt i           (1)
+    # ddot{θ} + b/J dot{θ} = Kt/J i
+    # ddot{θ} = -b/J dot{θ} + Kt/J i
+    # ddot{θ} = -b/J dot{θ} + Kt/J i + 0 V
+    #
+    # From Kirchoff's voltage law:
+    #
+    # L di/dt + R i = V - Ke dot{θ}         (2)
+    # di/dt + R/L i = V/L - Ke/L dot{θ}
+    # di/dt = -R/L i + V/L - Ke/L dot{θ}
+    # di/dt = -R/L i + 1/L V - Ke/L dot{θ}
+    # di/dt = -Ke/L dot{θ} - R/L i + 1/L V
+    A = np.array([[0, 1, 0], [0, -b / J, Kt / J], [0, -Ke / L, -R / L]])
+    B = np.array([[0], [0], [1 / L]])
+    C = np.array([[1, 0, 0]])
+    D = np.array([[0]])
+    K = np.array([[1, 0, 0]])
+    system = ct.StateSpace(A - B @ K, B @ K, C - D @ K, D @ K)
+
     plt.figure(2)
     plt.xlabel("Time ($s$)")
     plt.ylabel("Position ($m$)")
-    sim(ct.TransferFunction(1, 1), T, "Reference")
-    Gcl = make_closed_loop_plant(G, 1)
-    sim(Gcl, T, "Step response")
+
+    plt.plot(T, [1 for t in T], label="Reference")
+    plt.legend()
+
+    _, yout = ct.step_response(system, T=T, input=0, output=0)
+    plt.plot(T, yout, label="Step response")
+    plt.legend()
+
     if "--noninteractive" in sys.argv:
         latex.savefig("highfreq_unstable_step")
     else:
