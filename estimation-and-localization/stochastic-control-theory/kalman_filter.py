e0e73e71c63bfd1aad435e8af2de6992546436a8
Tyler Veness <calcmogul@gmail.com>
2023-03-19 22:35:45 -0700

    Make Kalman filter and smoother scripts use lists instead of 3D matrices

--- estimation-and-localization/stochastic-control-theory/kalman_filter.py
+++ estimation-and-localization/stochastic-control-theory/kalman_filter.py
@@ -17,71 +17,62 @@ plt.rc("text", usetex=True)
 
 def main():
     """Entry point."""
-    # x_1: robot position measured from corner
-    # x_2: robot velocity with positive direction toward wall
-    # x_3: wall position measured from corner
-    xhat = np.array([[0], [0], [0]])
-
-    # y_1: measurement of distance from robot to corner
-    # y_2: measurement of distance from robot to wall
+    # y₁: measurement of distance from robot to corner
+    # y₂: measurement of distance from robot to wall
     y = np.genfromtxt("kalman_robot.csv", delimiter=",")
+    measurements = y.shape[1]
 
-    # fmt: off
-    phi = np.array([[1, 1, 0],
-                    [0, 0, 0],
-                    [0, 0, 1]])
-
-    Q = np.diag(np.square([0, 0.1, 0]))
-    R = np.array([[10, 0],
-                   [0, 10]])
-
-    P = np.zeros((3, 3))
-    K = np.zeros((3, 2))
-    C = np.array([[1, 0, 0],
-                  [-1, 0, 1]])
-    # fmt: on
-
-    num_points = y.shape[1]
-    xhat_rec = np.zeros((3, 1, num_points))
-    P_rec = np.zeros((3, 3, num_points))
-    t = np.linspace(0, num_points, num_points)
+    A = np.array([[1, 1, 0], [0, 0, 0], [0, 0, 1]])
+    C = np.array([[1, 0, 0], [-1, 0, 1]])
 
     dt = 1
+    P = np.array(
+        [[10, 10 / dt, 10], [10 / dt, 20 / dt**2, 10 / dt], [10, 10 / dt, 20]]
+    )
+    Q = np.diag(np.square([0, 0.1, 0]))
+    R = np.array([[10, 0], [0, 10]])
 
-    for k in range(100):
-        if k == 1:
-            xhat[0] = y[0, k]
-            xhat[1] = (y[0, 1] - y[0, 0]) / dt
-            xhat[2] = y[0, k] + y[1, k]
-            # fmt: off
-            P = np.array([[10, 10 / dt, 10],
-                          [10 / dt, 20 / dt**2, 10 / dt],
-                          [10, 10 / dt, 20]])
-            # fmt: on
+    # Kalman filter storage
+    x_hat_rec = [np.zeros((3, 1)) for _ in range(measurements)]
+    P_rec = [np.zeros((3, 3)) for _ in range(measurements)]
 
-            xhat_rec[:, :, k] = xhat
-            P_rec[:, :, k] = np.array([P[0, 0], P[1, 1], P[2, 2]]).T
-        elif k > 1:
-            # Predict
-            xhat = phi @ xhat + np.array([[0, 0.8, 0]]).T
-            P = phi @ P @ phi.T + Q
+    # x₁: robot position measured from corner
+    # x₂: robot velocity with positive direction toward wall
+    # x₃: wall position measured from corner
+    x_hat = np.array([[y[0, 0]], [(y[0, 1] - y[0, 0]) / dt], [y[0, 0] + y[1, 0]]])
 
-            # Update
-            K = P @ C.T @ np.linalg.inv(C @ P @ C.T + R)
-            xhat += K @ (y[:, k : k + 1] - C @ xhat)
-            P = (np.eye(3, 3) - K @ C) @ P
+    x_hat_rec[0] = x_hat
+    P_rec[0] = P
 
-            xhat_rec[:, :, k] = xhat
-            P_rec[:, :, k] = np.array([P[0, 0], P[1, 1], P[2, 2]]).T
+    for k in range(1, measurements):
+        # Predict
+        x_hat = A @ x_hat + np.array([[0, 0.8, 0]]).T
+        P = A @ P @ A.T + Q
+
+        # Update
+        K = P @ C.T @ np.linalg.inv(C @ P @ C.T + R)
+        x_hat += K @ (y[:, k : k + 1] - C @ x_hat)
+        P = (np.eye(3) - K @ C) @ P
+
+        x_hat_rec[k] = x_hat
+        P_rec[k] = P
+
+    t = np.linspace(0, measurements, measurements)
 
     # State estimates and measurements
     plt.figure(1)
     plt.xlabel("Time (s)")
-    plt.plot(t[1:], xhat_rec[0, 0, 1:], label="Robot position estimate (cm)")
-    plt.plot(t[1:], xhat_rec[1, 0, 1:], label="Robot velocity estimate (cm/s)")
-    plt.plot(t[1:], xhat_rec[2, 0, 1:], label="Wall position estimate (cm)")
-    plt.plot(t[1:], y[0, 1:].T, label="Robot to corner measurement (cm)")
-    plt.plot(t[1:], y[1, 1:].T, label="Robot to wall measurement (cm)")
+    plt.plot(
+        t, [x_hat[0, 0] for x_hat in x_hat_rec], label="Robot position estimate (cm)"
+    )
+    plt.plot(
+        t, [x_hat[1, 0] for x_hat in x_hat_rec], label="Robot velocity estimate (cm/s)"
+    )
+    plt.plot(
+        t, [x_hat[2, 0] for x_hat in x_hat_rec], label="Wall position estimate (cm)"
+    )
+    plt.plot(t, y[0, :], label="Robot to corner measurement (cm)")
+    plt.plot(t, y[1, :], label="Robot to wall measurement (cm)")
     plt.legend()
     if "--noninteractive" in sys.argv:
         latex.savefig("kalman_filter_all")
@@ -89,8 +80,10 @@ def main():
     # Robot position estimate and variance
     plt.figure(2)
     plt.xlabel("Time (s)")
-    plt.plot(t[1:], xhat_rec[0, 0, 1:], label="Robot position estimate (cm)")
-    plt.plot(t[1:], P_rec[0, 0, 1:], label="Robot position variance ($cm^2$)")
+    plt.plot(
+        t, [x_hat[0, 0] for x_hat in x_hat_rec], label="Robot position estimate (cm)"
+    )
+    plt.plot(t, [P[0, 0] for P in P_rec], label="Robot position variance ($cm^2$)")
     plt.legend()
     if "--noninteractive" in sys.argv:
         latex.savefig("kalman_filter_robot_pos")
@@ -98,8 +91,10 @@ def main():
     # Wall position estimate and variance
     plt.figure(3)
     plt.xlabel("Time (s)")
-    plt.plot(t[1:], xhat_rec[2, 0, 1:], label="Wall position estimate (cm)")
-    plt.plot(t[1:], P_rec[2, 0, 1:], label="Wall position variance ($cm^2$)")
+    plt.plot(
+        t, [x_hat[2, 0] for x_hat in x_hat_rec], label="Wall position estimate (cm)"
+    )
+    plt.plot(t, [P[2, 0] for P in P_rec], label="Wall position variance ($cm^2$)")
     plt.legend()
     if "--noninteractive" in sys.argv:
         latex.savefig("kalman_filter_wall_pos")
--- estimation-and-localization/stochastic-control-theory/kalman_smoother.py
+++ estimation-and-localization/stochastic-control-theory/kalman_smoother.py
@@ -17,96 +17,65 @@ plt.rc("text", usetex=True)
 
 def main():
     """Entry point."""
-    # x_1: robot position measured from corner
-    # x_2: robot velocity with positive direction toward wall
-    # x_3: wall position measured from corner
-    xhat = np.array([[0.0], [0.0], [0.0]])
-
-    # y_1: measurement of distance from robot to corner
-    # y_2: measurement of distance from robot to wall
+    # y₁: measurement of distance from robot to corner
+    # y₂: measurement of distance from robot to wall
     y = np.genfromtxt("kalman_robot.csv", delimiter=",")
+    measurements = y.shape[1]
 
-    # fmt: off
-    A = np.array([[1, 1, 0],
-                  [0, 1, 0],
-                  [0, 0, 1]])
+    A = np.array([[1, 1, 0], [0, 1, 0], [0, 0, 1]])
+    C = np.array([[1, 0, 0], [-1, 0, 1]])
 
     Q = np.diag(np.square([0, 0.1, 0]))
-    R = np.array([[10, 0],
-                  [0, 10]])
+    R = np.array([[10, 0], [0, 10]])
 
-    P = np.zeros((3, 3))
-    K = np.zeros((3, 2))
-    C = np.array([[1, 0, 0],
-                  [-1, 0, 1]])
-    # fmt: on
+    # Kalman smoother storage
+    x_hat_pre = [np.zeros((3, 1)) for _ in range(measurements)]
+    x_hat_post = [np.zeros((3, 1)) for _ in range(measurements)]
+    P_pre = [np.zeros((3, 3)) for _ in range(measurements)]
+    P_post = [np.zeros((3, 3)) for _ in range(measurements)]
+    x_hat_smooth = [np.zeros((3, 1)) for _ in range(measurements)]
+    P_smooth = [np.zeros((3, 3)) for _ in range(measurements)]
 
-    # Initialize matrix storage
-    xhat_pre_rec = np.zeros((3, 1, y.shape[1]))
-    xhat_post_rec = np.zeros((3, 1, y.shape[1]))
-    P_pre_rec = np.zeros((3, 3, y.shape[1]))
-    P_post_rec = np.zeros((3, 3, y.shape[1]))
-    xhat_smooth_rec = np.zeros((3, 1, y.shape[1]))
-    P_smooth_rec = np.zeros((3, 3, y.shape[1]))
-    t = [0] * (y.shape[1])
+    # x₁: robot position measured from corner
+    # x₂: robot velocity with positive direction toward wall
+    # x₃: wall position measured from corner
+    x_hat_pre[0] = np.array([[y[0, 0]], [0.2], [y[0, 0] + y[1, 0]]])
+    x_hat_post[0] = np.array([[y[0, 0]], [0.2], [y[0, 0] + y[1, 0]]])
 
-    # Forward Kalman filter
+    P_pre[0] = np.array([[10, 0, 10], [0, 1, 0], [10, 0, 20]])
+    P_post[0] = np.array([[10, 0, 10], [0, 1, 0], [10, 0, 20]])
 
-    # Set up initial conditions for first measurement
-    xhat[0] = y[0, 0]
-    xhat[1] = 0.2
-    xhat[2] = y[0, 0] + y[1, 0]
-    # fmt: off
-    P = np.array([[10, 0, 10],
-                  [0, 1, 0],
-                  [10, 0, 20]])
-    # fmt: on
-
-    xhat_pre_rec[:, :, 1] = xhat
-    P_pre_rec[:, :, 1] = P
-    xhat_post_rec[:, :, 1] = xhat
-    P_post_rec[:, :, 1] = P
-    t[1] = 1
-
-    for k in range(2, 100):
+    # Filter
+    for k in range(measurements - 1):
         # Predict
-        xhat = A @ xhat
-        P = A @ P @ A.T + Q
-
-        xhat_pre_rec[:, :, k] = xhat
-        P_pre_rec[:, :, k] = P
+        x_hat_pre[k + 1] = A @ x_hat_post[k]
+        P_pre[k + 1] = A @ P_post[k] @ A.T + Q
 
         # Update
-        K = P @ C.T @ np.linalg.inv(C @ P @ C.T + R)
-        xhat += K @ (y[:, k : k + 1] - C @ xhat)
-        P = (np.eye(3, 3) - K @ C) @ P
-
-        xhat_post_rec[:, :, k] = xhat
-        P_post_rec[:, :, k] = P
-        t[k] = k
-
-    # Kalman smoother
-
-    # Last estimate is already optimal, so add it to the record
-    xhat_smooth_rec[:, :, -1] = xhat_post_rec[:, :, -1]
-    P_smooth_rec[:, :, -1] = P_post_rec[:, :, -1]
-
-    for k in range(y.shape[1] - 2, 0, -1):
-        K = P_post_rec[:, :, k] @ A.T @ np.linalg.inv(P_pre_rec[:, :, k + 1])
-        xhat = xhat_post_rec[:, :, k] + K @ (
-            xhat_smooth_rec[:, :, k + 1] - xhat_pre_rec[:, :, k + 1]
+        K = P_pre[k + 1] @ C.T @ np.linalg.inv(C @ P_pre[k + 1] @ C.T + R)
+        x_hat_post[k + 1] = x_hat_pre[k + 1] + K @ (
+            y[:, k + 1 : k + 2] - C @ x_hat_pre[k + 1]
         )
-        P = P_post_rec[:, :, k] + K @ (P - P_pre_rec[:, :, k + 1]) @ K.T
+        P_post[k + 1] = (np.eye(3) - K @ C) @ P_pre[k + 1]
 
-        xhat_smooth_rec[:, :, k] = xhat
-        P_smooth_rec[:, :, k] = P
+    # Last filtered estimate is already optimal smoothed estimate
+    x_hat_smooth[-1] = x_hat_post[-1]
+    P_smooth[-1] = P_post[-1]
+
+    # Smoother
+    for k in range(measurements - 2, -1, -1):
+        K = P_post[k] @ A.T @ np.linalg.inv(P_pre[k + 1])
+        x_hat_smooth[k] = x_hat_post[k] + K @ (x_hat_smooth[k + 1] - x_hat_pre[k + 1])
+        P_smooth[k] = P_post[k] + K @ (P_smooth[k + 1] - P_pre[k + 1]) @ K.T
+
+    t = np.linspace(0, measurements, measurements)
 
     # Robot position
     plt.figure(1)
     plt.xlabel("Time (s)")
     plt.ylabel("Position (cm)")
-    plt.plot(t[1:], xhat_post_rec[0, 0, 1:], label="Kalman filter")
-    plt.plot(t[1:], xhat_smooth_rec[0, 0, 1:], label="Kalman smoother")
+    plt.plot(t, [x_hat[0, 0] for x_hat in x_hat_post], label="Kalman filter")
+    plt.plot(t, [x_hat[0, 0] for x_hat in x_hat_smooth], label="Kalman smoother")
     plt.legend()
     if "--noninteractive" in sys.argv:
         latex.savefig("kalman_smoother_robot_pos")
@@ -115,8 +84,8 @@ def main():
     plt.figure(2)
     plt.xlabel("Time (s)")
     plt.ylabel("Velocity (cm/s)")
-    plt.plot(t[1:], xhat_post_rec[1, 0, 1:], label="Kalman filter")
-    plt.plot(t[1:], xhat_smooth_rec[1, 0, 1:], label="Kalman smoother")
+    plt.plot(t, [x_hat[1, 0] for x_hat in x_hat_post], label="Kalman filter")
+    plt.plot(t, [x_hat[1, 0] for x_hat in x_hat_smooth], label="Kalman smoother")
     plt.legend()
     if "--noninteractive" in sys.argv:
         latex.savefig("kalman_smoother_robot_vel")
@@ -125,8 +94,8 @@ def main():
     plt.figure(3)
     plt.xlabel("Time (s)")
     plt.ylabel("Wall position (cm)")
-    plt.plot(t[1:], xhat_post_rec[2, 0, 1:], label="Kalman filter")
-    plt.plot(t[1:], xhat_smooth_rec[2, 0, 1:], label="Kalman smoother")
+    plt.plot(t, [x_hat[2, 0] for x_hat in x_hat_post], label="Kalman filter")
+    plt.plot(t, [x_hat[2, 0] for x_hat in x_hat_smooth], label="Kalman smoother")
     plt.legend()
     if "--noninteractive" in sys.argv:
         latex.savefig("kalman_smoother_wall_pos")
@@ -135,8 +104,8 @@ def main():
     plt.figure(4)
     plt.xlabel("Time (s)")
     plt.ylabel("Robot position variance ($cm^2$)")
-    plt.plot(t[1:], P_post_rec[1, 1, 1:], label="Kalman filter")
-    plt.plot(t[1:], P_smooth_rec[1, 1, 1:], label="Kalman smoother")
+    plt.plot(t, [P[1, 1] for P in P_post], label="Kalman filter")
+    plt.plot(t, [P[1, 1] for P in P_smooth], label="Kalman smoother")
     plt.legend()
     if "--noninteractive" in sys.argv:
         latex.savefig("kalman_smoother_robot_pos_variance")
