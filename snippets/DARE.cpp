9ac486d6dd8d9eceb4f1b7578e81ae9f078d702f
Tyler Veness <calcmogul@gmail.com>
2023-08-12 13:17:52 -0700

    Shorten DARE solver snippet

--- snippets/DARE.cpp
+++ snippets/DARE.cpp
@@ -1,6 +1,6 @@
 #include <Eigen/Cholesky>
 #include <Eigen/Core>
-#include <Eigen/QR>
+#include <Eigen/LU>
 
 Eigen::MatrixXd DARE(const Eigen::Ref<const Eigen::MatrixXd>& A,
                      const Eigen::Ref<const Eigen::MatrixXd>& B,
@@ -12,39 +12,25 @@ Eigen::MatrixXd DARE(const Eigen::Ref<const Eigen::MatrixXd>& A,
   //     International Journal of Control, 77:8, 767-788, 2004.
   //     DOI: 10.1080/00207170410001714988
   //
-  // Implements the SSCA algorithm on page 12 of [1]. See equation (4) of [1]
-  // for initial values of A, G, and H.
-
+  // Implements SDA algorithm on p. 5 of [1] (initial A, G, H are from (4)).
   Eigen::MatrixXd A_k = A;
-  Eigen::MatrixXd A_k1 = A;
-
   Eigen::MatrixXd G_k = B * R.llt().solve(B.transpose());
-
-  Eigen::MatrixXd I = Eigen::MatrixXd::Identity(A.rows(), A.cols());
-
-  Eigen::MatrixXd H_k = Q;
+  Eigen::MatrixXd H_k;
   Eigen::MatrixXd H_k1 = Q;
 
   do {
-    A_k = A_k1;
     H_k = H_k1;
 
-    Eigen::MatrixXd W = I + H_k * G_k;
+    Eigen::MatrixXd W =
+        Eigen::MatrixXd::Identity(H_k.rows(), H_k.cols()) + G_k * H_k;
 
-    // W is symmetric positive definite, but LLT solver [2] + iterative
-    // refinement to fix inaccuracy [3] was slower than just householder QR
-    // solver.
-    //
-    // [2] https://eigen.tuxfamily.org/dox/group__TutorialLinearAlgebra.html
-    // [3] https://en.wikipedia.org/wiki/Iterative_refinement
-    auto W_solver = W.householderQr();
+    auto W_solver = W.lu();
+    Eigen::MatrixXd V_1 = W_solver.solve(A_k);
+    Eigen::MatrixXd V_2 = W_solver.solve(G_k.transpose()).transpose();
 
-    Eigen::MatrixXd V_1 = W_solver.solve(A_k.transpose());
-    Eigen::MatrixXd V_2 = W_solver.solve(H_k);
-
-    A_k1 = V_1.transpose() * A_k;
-    G_k += A_k * G_k * V_1;
-    H_k1 = H_k + A_k.transpose() * V_2 * A_k;
+    G_k += A_k * V_2 * A_k.transpose();
+    H_k1 = H_k + V_1.transpose() * H_k * A_k;
+    A_k *= V_1;
   } while ((H_k1 - H_k).norm() > 1e-10 * H_k1.norm());
 
   return H_k1;
