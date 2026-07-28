121fff1d066d4b2b1de55b82e303f661c3f3ed1f
Tyler Veness <calcmogul@gmail.com>
2026-07-27 18:45:11 -0700

    Fix adaptive stepping in RKDP snippet

--- snippets/rkdp.cpp
+++ snippets/rkdp.cpp
@@ -2,8 +2,6 @@
 #include <array>
 #include <cmath>
 
-#include <Eigen/Core>
-
 /// Performs adaptive Dormand-Prince integration of dx/dt = f(x, u) for dt.
 ///
 /// @param f The function to integrate. It must take two arguments x and u.
@@ -37,44 +35,44 @@ T rkdp(F&& f, T x, U u, double dt, double max_error = 1e-6) {
                                        -92097.0 / 339200.0, 187.0 / 2100.0,
                                        1.0 / 40.0};
 
-  T new_x;
-  double truncation_error;
-
+  // Loop until dt has elapsed
   double dt_elapsed = 0.0;
   double h = dt;
-
-  // Loop until we've gotten to our desired dt
   while (dt_elapsed < dt) {
-    do {
-      // Only allow us to advance up to the dt remaining
-      h = std::min(h, dt - dt_elapsed);
+    // clang-format off
+    T k1 = f(x, u);
+    T k2 = f(x + h * (A[0][0] * k1), u);
+    T k3 = f(x + h * (A[1][0] * k1 + A[1][1] * k2), u);
+    T k4 = f(x + h * (A[2][0] * k1 + A[2][1] * k2 + A[2][2] * k3), u);
+    T k5 = f(x + h * (A[3][0] * k1 + A[3][1] * k2 + A[3][2] * k3 + A[3][3] * k4), u);
+    T k6 = f(x + h * (A[4][0] * k1 + A[4][1] * k2 + A[4][2] * k3 + A[4][3] * k4 + A[4][4] * k5), u);
+    // clang-format on
 
-      // clang-format off
-      T k1 = f(x, u);
-      T k2 = f(x + h * (A[0][0] * k1), u);
-      T k3 = f(x + h * (A[1][0] * k1 + A[1][1] * k2), u);
-      T k4 = f(x + h * (A[2][0] * k1 + A[2][1] * k2 + A[2][2] * k3), u);
-      T k5 = f(x + h * (A[3][0] * k1 + A[3][1] * k2 + A[3][2] * k3 + A[3][3] * k4), u);
-      T k6 = f(x + h * (A[4][0] * k1 + A[4][1] * k2 + A[4][2] * k3 + A[4][3] * k4 + A[4][4] * k5), u);
-      // clang-format on
-
-      // Since the final row of A and the array b1 have the same coefficients
-      // and k7 has no effect on new_x, we can reuse the calculation.
-      new_x = x + h * (A[5][0] * k1 + A[5][1] * k2 + A[5][2] * k3 +
+    // Since the final row of A and the array b1 have the same coefficients and
+    // k7 has no effect on new_x, we can reuse the calculation.
+    T new_x = x + h * (A[5][0] * k1 + A[5][1] * k2 + A[5][2] * k3 +
                        A[5][3] * k4 + A[5][4] * k5 + A[5][5] * k6);
-      T k7 = f(new_x, u);
+    T k7 = f(new_x, u);
 
-      truncation_error = (h * ((b1[0] - b2[0]) * k1 + (b1[1] - b2[1]) * k2 +
-                               (b1[2] - b2[2]) * k3 + (b1[3] - b2[3]) * k4 +
-                               (b1[4] - b2[4]) * k5 + (b1[5] - b2[5]) * k6 +
-                               (b1[6] - b2[6]) * k7))
-                             .norm();
+    double truncation_error =
+        (h *
+         ((b1[0] - b2[0]) * k1 + (b1[1] - b2[1]) * k2 + (b1[2] - b2[2]) * k3 +
+          (b1[3] - b2[3]) * k4 + (b1[4] - b2[4]) * k5 + (b1[5] - b2[5]) * k6 +
+          (b1[6] - b2[6]) * k7))
+            .norm();
 
-      h *= 0.9 * std::pow(max_error / truncation_error, 1.0 / 5.0);
-    } while (truncation_error > max_error);
+    if (truncation_error <= max_error) {
+      // Accept the step
+      x = new_x;
+      dt_elapsed += h;
+    }
 
-    dt_elapsed += h;
-    x = new_x;
+    if (truncation_error == 0.0) {
+      h = dt - dt_elapsed;
+    } else {
+      h = std::min(0.9 * h * std::pow(max_error / truncation_error, 0.2),
+                   dt - dt_elapsed);
+    }
   }
 
   return x;
